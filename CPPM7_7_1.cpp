3c2ca6ecace907217df9778060641a9d907b1229
GarDeN <garpinich22@mail.ru>
2023-02-16 14:33:47 +0300

    corrected version

--- CPPM7_7_1.cpp
+++ CPPM7_7_1.cpp
@@ -1,15 +1,18 @@
 #include "CPPM7_7_1.h"
 #define MODE 1
-
-void CPPM7_7_1() {
 #ifndef MODE
 #error You need to define MODE
-#else
+#elif (MODE == 1)
+int add(int num1, int num2) {
+	return num1 + num2;
+}
+#endif
+
+void CPPM7_7_1() {
 #if (MODE == 0)
 	std::cout << "Работаю в боевом режиме\n";
 #elif (MODE == 1)
 	std::cout << "Работаю в боевом режиме\n";
-	int add(int num1, int num2);
 int num1, num2;
 std::cout << "Введите число 1: ";
 std::cin >> num1;
@@ -17,9 +20,5 @@ std::cout << "Введите число 2: ";
 std::cin >> num2;
 std::cout << "Результат сложения: " << add(num1, num2) << '\n';
 #endif
-#endif
 }
 
-int add(int num1, int num2) {
-	return num1 + num2;
-}
